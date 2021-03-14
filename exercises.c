edbfae0a407dcd9c6c62cc943a2734b65da13c78
Replit user <replituser@example.com>
2021-03-14 20:04:56 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -39,7 +39,9 @@ Utilice la función sumaN.
 void sumaNultimos(int a[], int n, int m, int * suma) {
     int i;
     int tot = 0;
-    for(i = n; i > m; i--){
+    int max;
+    max = n - m;
+    for(i = n-1; i >= max; i--){
       printf("%d\n",a[i]);
       tot += a[i];
     }
